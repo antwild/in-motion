6303f3eb2c3c79bf368b904e5ff8dba30cf5bf94
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-07-05 15:37:07 +0100

    Mailer for preconsultation set up

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -7,12 +7,14 @@ class EnquiryMailer < ApplicationMailer
   #
   def send_precon
     @enquiry = params[:enquiry]
+    # NEED TO SET FOR PRODUCTION
     @url = "http://localhost:3000/preconsultations/new"
     mail(to: @enquiry[:email], subject: "Book your In Motion Food Company consultation")
   end
 
   def new_enq
     @enquiry = params[:enquiry]
-    mail(to: "antwildy@gmail.com", subject: "New enquiry for a preconsultation form")
+    # NEED TO SET EMAIL FOR PRODUCTION
+    mail(to: ENV['NEEDTOSET'], subject: "New enquiry for a preconsultation form")
   end
 end
